fe84f3656296c730fbedceb26c22233dd69674dc
Alun Turner <alun.turner@googlemail.com>
2020-04-16 13:46:16 +0100

    complete refactor add comments model

--- routers/articles.js
+++ routers/articles.js
@@ -1,13 +1,17 @@
 const articlesRouter = require("express").Router();
 const { getArticleById, patchArticleById } = require("../controllers/articles");
 const { invalidMethodController } = require("../errors");
-const commentsRouter = require("./comments");
+const { postCommentByArticleId } = require("../controllers/comments");
 
-articlesRouter.use("/:article_id/comments", commentsRouter);
 articlesRouter
   .route("/:article_id")
   .get(getArticleById)
   .patch(patchArticleById)
+  .all(invalidMethodController);
+
+articlesRouter
+  .route("/:article_id/comments")
+  .post(postCommentByArticleId)
   .all(invalidMethodController);
 
 module.exports = articlesRouter;
--- routers/comments.js
+++
@@ -1,6 +0,0 @@
-const commentsRouter = require("express").Router();
-const { postCommentByArticleId } = require("../controllers/comments");
-
-commentsRouter.route("/").post(postCommentByArticleId);
-
-module.exports = commentsRouter;

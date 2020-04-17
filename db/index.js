e30299b22376c3f663cd15d83dd6f8931611fffb
Alun Turner <alun.turner@googlemail.com>
2020-04-17 14:38:49 +0100

    working on heroku hosting

--- db/index.js
+++ db/index.js
@@ -1,5 +1,12 @@
-const dbConfig = require("../knexfile");
+const ENV = process.env.NODE_ENV || "development";
+const knex = require("knex");
 
-const knex = require("knex")(dbConfig);
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
 
-module.exports = knex;
+module.exports = knex(dbConfig);
+// const dbConfig = require("../knexfile");
+// const knex = require("knex")(dbConfig);
+// module.exports = knex;
